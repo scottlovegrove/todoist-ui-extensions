713159c885809859d857baba246e4254a82c09fa
Scott Lovegrove <scott@ferretlabs.com>
2022-11-19 08:11:19 +0000

    chore: *Real* first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,13 +15,26 @@ module.exports = {
     root: true,
     ignorePatterns: ['scripts/', '.eslintrc.js', 'global-setup.js', 'jest.config.js'],
     rules: {
+        '@typescript-eslint/no-useless-constructor': 'error',
         '@typescript-eslint/no-unnecessary-condition': 'error',
+        'no-restricted-imports': [
+            'error',
+            {
+                patterns: [
+                    {
+                        group: ['@doist/ui-extensions-server/dist/*'],
+                        message: 'Import directly from the package root, not the "dist" directory',
+                    },
+                ],
+            },
+        ],
     },
     overrides: [
         {
             files: ['*.spec.*', 'fixtures.ts'],
             rules: {
                 '@typescript-eslint/unbound-method': 'off',
+                'no-restricted-imports': 'off',
             },
         },
     ],
--- src/utils/header-utils.ts
+++
@@ -1,5 +0,0 @@
-const VERIFICATION_TOKENS = process.env.VERIFICATION_TOKENS
-
-export function isValidHeader() {
-    console.log({ VERIFICATION_TOKENS })
-}
